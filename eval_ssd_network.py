FIX: Bug in L2 Normalization in NCHW mode.

eval_ssd_network.py
@@ -35,6 +35,7 @@ slim = tf.contrib.slim
 # List of recalls values at which precision is evaluated.
 LIST_RECALLS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85,
                 0.90, 0.95, 0.96, 0.97, 0.98, 0.99]
+DATA_FORMAT = 'NCHW'
 
 # =========================================================================== #
 # SSD evaluation Flags.
@@ -148,8 +149,10 @@ def main(_):
             image, glabels, gbboxes, gbbox_img = \
                 image_preprocessing_fn(image, glabels, gbboxes,
                                        out_shape=ssd_shape,
+                                       data_format=DATA_FORMAT,
                                        resize=FLAGS.eval_resize,
                                        difficults=gdifficult)
+
             # Encode groundtruth labels and bboxes.
             gclasses, glocalisations, gscores = \
                 ssd_net.bboxes_encode(glabels, gbboxes, ssd_anchors)
@@ -170,7 +173,7 @@ def main(_):
         # SSD Network + Ouputs decoding.
         # =================================================================== #
         dict_metrics = {}
-        arg_scope = ssd_net.arg_scope()
+        arg_scope = ssd_net.arg_scope(data_format=DATA_FORMAT)
         with slim.arg_scope(arg_scope):
             predictions, localisations, logits, end_points = \
                 ssd_net.net(b_image, is_training=False)

nets/custom_layers.py
@@ -79,17 +79,17 @@ def l2_normalization(
 
     with variable_scope.variable_scope(
             scope, 'L2Normalization', [inputs], reuse=reuse) as sc:
-
         inputs_shape = inputs.get_shape()
         inputs_rank = inputs_shape.ndims
         dtype = inputs.dtype.base_dtype
         if data_format == 'NHWC':
+            norm_dim = tf.range(1, inputs_rank-1)
             params_shape = inputs_shape[-1:]
         elif data_format == 'NCHW':
+            norm_dim = tf.range(2, inputs_rank)
             params_shape = (inputs_shape[1])
 
         # Normalize along spatial dimensions.
-        norm_dim = tf.range(1, inputs_rank-1)
         outputs = nn.l2_normalize(inputs, norm_dim, epsilon=1e-12)
         # Additional scaling.
         if scaling:
@@ -101,11 +101,14 @@ def l2_normalization(
                                              initializer=scale_initializer,
                                              collections=scale_collections,
                                              trainable=trainable)
-            if data_format == 'NCHW':
+            if data_format == 'NHWC':
+                outputs = tf.multiply(outputs, scale)
+            elif data_format == 'NCHW':
                 scale = tf.expand_dims(scale, axis=-1)
                 scale = tf.expand_dims(scale, axis=-1)
+                outputs = tf.multiply(outputs, scale)
+                # outputs = tf.transpose(outputs, perm=(0, 2, 3, 1))
 
-            outputs = tf.multiply(outputs, scale)
         return utils.collect_named_outputs(outputs_collections,
                                            sc.original_name_scope, outputs)
 

nets/ssd_vgg_300.py
@@ -474,15 +474,13 @@ def ssd_net(inputs,
         with tf.variable_scope(end_point):
             net = slim.conv2d(net, 256, [1, 1], scope='conv1x1')
             net = custom_layers.pad2d(net, pad=(1, 1))
-            net = slim.conv2d(net, 512, [3, 3], stride=2, scope='conv3x3',
-                              padding='VALID')
+            net = slim.conv2d(net, 512, [3, 3], stride=2, scope='conv3x3', padding='VALID')
         end_points[end_point] = net
         end_point = 'block9'
         with tf.variable_scope(end_point):
             net = slim.conv2d(net, 128, [1, 1], scope='conv1x1')
             net = custom_layers.pad2d(net, pad=(1, 1))
-            net = slim.conv2d(net, 256, [3, 3], stride=2, scope='conv3x3',
-                              padding='VALID')
+            net = slim.conv2d(net, 256, [3, 3], stride=2, scope='conv3x3', padding='VALID')
         end_points[end_point] = net
         end_point = 'block10'
         with tf.variable_scope(end_point):
@@ -531,7 +529,8 @@ def ssd_arg_scope(weight_decay=0.0005, data_format='NHWC'):
         with slim.arg_scope([slim.conv2d, slim.max_pool2d],
                             padding='SAME',
                             data_format=data_format):
-            with slim.arg_scope([custom_layers.pad2d,
+            with slim.arg_scope([slim.conv2d, slim.max_pool2d,
+                                 custom_layers.pad2d,
                                  custom_layers.l2_normalization,
                                  custom_layers.channel_to_last],
                                 data_format=data_format) as sc:

tf_extended/math.py
@@ -60,8 +60,8 @@ def cummax(x, reverse=False, name=None):
             x = tf.reverse(x, axis=[0])
         # 'Accumlating' maximum: ensure it is always increasing.
         cmax = tf.scan(lambda a, y: tf.maximum(a, y), x,
-                       initializer=None, parallel_iterations=10,
-                       back_prop=True, swap_memory=False)
+                       initializer=None, parallel_iterations=1,
+                       back_prop=False, swap_memory=False)
         if reverse:
             cmax = tf.reverse(cmax, axis=[0])
         return cmax