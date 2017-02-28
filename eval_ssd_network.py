FIX: L2 normalization on channels instead of spatial dimensions.
Should now reproduce expected results!

eval_ssd_network.py
@@ -187,7 +187,7 @@ def main(_):
                        b_gclasses, b_glocalisations, b_gscores)
 
         # Performing post-processing on CPU: loop-intensive, usually more efficient.
-        with tf.device('/cpu:0'):
+        with tf.device('/device:CPU:0'):
             # Detected objects from SSD output.
             localisations = ssd_net.bboxes_decode(localisations, ssd_anchors)
             rscores, rbboxes = \
@@ -216,7 +216,7 @@ def main(_):
         # =================================================================== #
         # Evaluation metrics.
         # =================================================================== #
-        with tf.device('/cpu:0'):
+        with tf.device('/device:CPU:0'):
             dict_metrics = {}
             # First add all losses.
             for loss in tf.get_collection(tf.GraphKeys.LOSSES):
@@ -290,6 +290,8 @@ def main(_):
         # =================================================================== #
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=FLAGS.gpu_memory_fraction)
         config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
+        # config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+
         # Number of batches...
         if FLAGS.max_num_batches:
             num_batches = FLAGS.max_num_batches

nets/custom_layers.py
@@ -83,10 +83,12 @@ def l2_normalization(
         inputs_rank = inputs_shape.ndims
         dtype = inputs.dtype.base_dtype
         if data_format == 'NHWC':
-            norm_dim = tf.range(1, inputs_rank-1)
+            # norm_dim = tf.range(1, inputs_rank-1)
+            norm_dim = tf.range(inputs_rank-1, inputs_rank)
             params_shape = inputs_shape[-1:]
         elif data_format == 'NCHW':
-            norm_dim = tf.range(2, inputs_rank)
+            # norm_dim = tf.range(2, inputs_rank)
+            norm_dim = tf.range(1, 2)
             params_shape = (inputs_shape[1])
 
         # Normalize along spatial dimensions.