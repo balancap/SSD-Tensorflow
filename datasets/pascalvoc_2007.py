2ff63fd283dae786ab00255d833e3dd1f574b36f
Paul Balanca <paul.balanca@gmail.com>
2017-04-10 12:21:07 +0100

    ADD: Pascal VOC 07 & 12 statistics.

--- datasets/pascalvoc_2007.py
+++ datasets/pascalvoc_2007.py
@@ -26,9 +26,62 @@ ITEMS_TO_DESCRIPTIONS = {
     'object/bbox': 'A list of bounding boxes, one per each object.',
     'object/label': 'A list of labels, one per each object.',
 }
+# (Images, Objects) statistics on every class.
+TRAIN_STATISTICS = {
+    'none': (0, 0),
+    'aeroplane': (238, 306),
+    'bicycle': (243, 353),
+    'bird': (330, 486),
+    'boat': (181, 290),
+    'bottle': (244, 505),
+    'bus': (186, 229),
+    'car': (713, 1250),
+    'cat': (337, 376),
+    'chair': (445, 798),
+    'cow': (141, 259),
+    'diningtable': (200, 215),
+    'dog': (421, 510),
+    'horse': (287, 362),
+    'motorbike': (245, 339),
+    'person': (2008, 4690),
+    'pottedplant': (245, 514),
+    'sheep': (96, 257),
+    'sofa': (229, 248),
+    'train': (261, 297),
+    'tvmonitor': (256, 324),
+    'total': (5011, 12608),
+}
+TEST_STATISTICS = {
+    'none': (0, 0),
+    'aeroplane': (1, 1),
+    'bicycle': (1, 1),
+    'bird': (1, 1),
+    'boat': (1, 1),
+    'bottle': (1, 1),
+    'bus': (1, 1),
+    'car': (1, 1),
+    'cat': (1, 1),
+    'chair': (1, 1),
+    'cow': (1, 1),
+    'diningtable': (1, 1),
+    'dog': (1, 1),
+    'horse': (1, 1),
+    'motorbike': (1, 1),
+    'person': (1, 1),
+    'pottedplant': (1, 1),
+    'sheep': (1, 1),
+    'sofa': (1, 1),
+    'train': (1, 1),
+    'tvmonitor': (1, 1),
+    'total': (20, 20),
+}
 SPLITS_TO_SIZES = {
     'train': 5011,
     'test': 4952,
+}
+SPLITS_TO_STATISTICS = {
+    'train': TRAIN_STATISTICS,
+    'test': TEST_STATISTICS,
 }
 NUM_CLASSES = 20
 
--- datasets/pascalvoc_2012.py
+++ datasets/pascalvoc_2012.py
@@ -26,8 +26,36 @@ ITEMS_TO_DESCRIPTIONS = {
     'object/bbox': 'A list of bounding boxes, one per each object.',
     'object/label': 'A list of labels, one per each object.',
 }
+# (Images, Objects) statistics on every class.
+TRAIN_STATISTICS = {
+    'none': (0, 0),
+    'aeroplane': (670, 865),
+    'bicycle': (552, 711),
+    'bird': (765, 1119),
+    'boat': (508, 850),
+    'bottle': (706, 1259),
+    'bus': (421, 593),
+    'car': (1161, 2017),
+    'cat': (1080, 1217),
+    'chair': (1119, 2354),
+    'cow': (303, 588),
+    'diningtable': (538, 609),
+    'dog': (1286, 1515),
+    'horse': (482, 710),
+    'motorbike': (526, 713),
+    'person': (4087, 8566),
+    'pottedplant': (527, 973),
+    'sheep': (325, 813),
+    'sofa': (507, 566),
+    'train': (544, 628),
+    'tvmonitor': (575, 784),
+    'total': (11540, 27450),
+}
 SPLITS_TO_SIZES = {
     'train': 17125,
+}
+SPLITS_TO_STATISTICS = {
+    'train': TRAIN_STATISTICS,
 }
 NUM_CLASSES = 20
 
